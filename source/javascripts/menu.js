5788597cf65c6e07827a1c35e178f6745e499b44
germainloret <germain.loret@gmail.com>
2017-06-12 18:52:12 +0200

    few change

--- source/javascripts/menu.js
+++ source/javascripts/menu.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $('.menu-content .menu-toggle').on('click', function(){
     $(this).toggleClass('on');
-    $('.menu-content > nav > ul').toggleClass('hidden');
+    $('.menu-content > nav > ul').toggleClass('mask');
   });
 });
