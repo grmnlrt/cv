7c80344f4518ff86ba221905eaaba430c1a29490
germainloret <germain.loret@gmail.com>
2017-06-17 16:59:01 +0200

    finalize home

--- source/javascripts/menu.js
+++ source/javascripts/menu.js
@@ -2,5 +2,6 @@ $(document).ready(function(){
   $('.menu-content .menu-toggle').on('click', function(){
     $(this).toggleClass('on');
     $('.menu-content > nav > ul').toggleClass('mask');
+    $('.menu-content .home').toggle().hidden();
   });
 });
